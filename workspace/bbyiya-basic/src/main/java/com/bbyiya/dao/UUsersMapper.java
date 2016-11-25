bfa3a09b182a78d35b1896368594d5ccdf9d29b8
zy <okmen.zy@foxmail.com>
2016-11-25 16:59:15 +0800

    sss

--- workspace/bbyiya-basic/src/main/java/com/bbyiya/dao/UUsersMapper.java
+++ workspace/bbyiya-basic/src/main/java/com/bbyiya/dao/UUsersMapper.java
@@ -3,14 +3,27 @@ package com.bbyiya.dao;
 import com.bbyiya.model.UUsers;
 
 public interface UUsersMapper {
+	/**
+	 * 删除用户
+	 * @param userid
+	 * @return
+	 */
 	int deleteByPrimaryKey(Long userid);
-
+	/**
+	 * 新增用户
+	 * @param record
+	 * @return
+	 */
 	int insert(UUsers record);
-	
+	/**
+	 * 新增用户 返回自增主键userId
+	 * @param record
+	 * @return
+	 */
 	int insertReturnKeyId(UUsers record);
 
 	int insertSelective(UUsers record);
-
+	
 	UUsers selectByPrimaryKey(Long userid);
 
 	UUsers getUUsersByUserID(Long userid);

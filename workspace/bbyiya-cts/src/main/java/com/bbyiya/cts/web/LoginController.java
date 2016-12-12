03ffb9ebd008f7f6dd9c7e985d09db02193f9258
Administrator <Administrator@win-10.lan>
2016-12-12 16:11:36 +0800

    cts

--- workspace/bbyiya-cts/src/main/java/com/bbyiya/cts/web/LoginController.java
+++ workspace/bbyiya-cts/src/main/java/com/bbyiya/cts/web/LoginController.java
@@ -1,11 +1,40 @@
 package com.bbyiya.cts.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bbyiya.dao.UUsersMapper;
+import com.bbyiya.model.UUsers;
+import com.bbyiya.utils.ObjectUtil;
+import com.bbyiya.utils.RedisUtil;
 
 
 @Controller
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/")
 public class LoginController {
-
+	@Autowired
+	private UUsersMapper userMapper;
+	
+	
+	@RequestMapping(value = "/login")
+	public String login(Model model,String userno) throws Exception {
+		String key="userid_"+userno;
+		UUsers user=(UUsers)RedisUtil.getObject(key);// 
+		if(user==null){
+			user=userMapper.getUUsersByUserID(ObjectUtil.parseLong(userno));
+			if(user!=null){
+				RedisUtil.setObject(key, user,120);	
+			}
+		}else {
+			model.addAttribute("isok", "ok");
+		}
+		return "login";
+	}
+	
+	@RequestMapping(value = "/register")
+	public String register(Model model,String aa) throws Exception {
+		return "register";
+	}
 }

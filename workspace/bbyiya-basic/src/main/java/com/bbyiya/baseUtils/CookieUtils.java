0bf17efc1a6e1482ca02235ec48543c7ee69e36d
okmen <okmen.zy@foxmail.com>
2017-02-10 16:33:55 +0800

    cookie

--- workspace/bbyiya-basic/src/main/java/com/bbyiya/baseUtils/CookieUtils.java
+++ workspace/bbyiya-basic/src/main/java/com/bbyiya/baseUtils/CookieUtils.java
@@ -7,21 +7,74 @@ import com.bbyiya.utils.ObjectUtil;
 
 public class CookieUtils {
 
+//	/**
+//	 * 获取指定cookie值
+//	 * 
+//	 * @param request  请求实体
+//	 * @param name cookie的键
+//	 * @return cookie的值
+//	 */
+//	public static String getCookieByName(HttpServletRequest request, String name) {
+//		Cookie[] cookies = request.getCookies();
+//		if (ObjectUtil.isEmpty(name)) {
+//			return null;
+//		}
+//		String sessionId= request.getSession().getId();
+//		System.out.println(sessionId); 
+//		if (cookies != null) {
+//			for (Cookie cookie : cookies) {
+//				if (name.equals(cookie.getName().trim())) {
+//					return cookie.getValue();
+//				}
+//			}
+//			return null;
+//		} else {
+//			return null;
+//		}
+//	}
+//
+//	/**
+//	 * 设置Cookie
+//	 * @param response 
+//	 * @param name cookie 名称
+//	 * @param value 
+//	 * @param maxAge 有效时间
+//	 */
+//	public static void addCookie(HttpServletResponse response, String name,String value, int maxAge) {
+//		Cookie cookie = new Cookie(name, value);
+//		cookie.setPath("/");
+//		if (maxAge > 0)
+//			cookie.setMaxAge(maxAge);
+//		response.addCookie(cookie);
+//	}
+	
 	/**
-	 * 获取指定cookie值
-	 * 
-	 * @param request  请求实体
-	 * @param name cookie的键
-	 * @return cookie的值
+	 * 通过SessionId设置Cookie
+	 * @param request
+	 * @param response
+	 * @param value
+	 * @param maxAge
 	 */
-	public static String getCookieByName(HttpServletRequest request, String name) {
+	public static void addCookieBySessionId(HttpServletRequest request, HttpServletResponse response, String value, int maxAge) {
+		String sessionId= request.getSession().getId();
+		Cookie cookie = new Cookie(sessionId, value);
+		cookie.setPath("/");
+		if (maxAge > 0)
+			cookie.setMaxAge(maxAge);
+		response.addCookie(cookie);
+	}
+	
+	/**
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static String getCookieBySessionId(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
-		if (ObjectUtil.isEmpty(name)) {
-			return null;
-		}
+		String sessionId= request.getSession().getId();
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
-				if (name.equals(cookie.getName().trim())) {
+				if (sessionId.equals(cookie.getName().trim())) {
 					return cookie.getValue();
 				}
 			}
@@ -30,19 +83,5 @@ public class CookieUtils {
 			return null;
 		}
 	}
-
-	/**
-	 * 设置Cookie
-	 * @param response 
-	 * @param name cookie 名称
-	 * @param value 
-	 * @param maxAge 有效时间
-	 */
-	public static void addCookie(HttpServletResponse response, String name,String value, int maxAge) {
-		Cookie cookie = new Cookie(name, value);
-		cookie.setPath("/");
-		if (maxAge > 0)
-			cookie.setMaxAge(maxAge);
-		response.addCookie(cookie);
-	}
+	
 }

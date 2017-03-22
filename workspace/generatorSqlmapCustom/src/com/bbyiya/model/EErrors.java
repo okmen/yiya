acfd29296f096bcd36cf63eebab162b1261d4f28
okmen <okmen.zy@foxmail.com>
2017-03-22 19:01:18 +0800

    sss

---
+++ workspace/generatorSqlmapCustom/src/com/bbyiya/model/EErrors.java
@@ -0,0 +1,45 @@
+package com.bbyiya.model;
+
+import java.util.Date;
+
+public class EErrors {
+    private Long logid;
+
+    private String classname;
+
+    private String msg;
+
+    private Date createtime;
+
+    public Long getLogid() {
+        return logid;
+    }
+
+    public void setLogid(Long logid) {
+        this.logid = logid;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname == null ? null : classname.trim();
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg == null ? null : msg.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+}

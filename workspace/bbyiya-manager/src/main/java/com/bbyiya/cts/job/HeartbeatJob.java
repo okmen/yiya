5597dcb2984be1a95a062c3431c37ffdf1b82090
okmen <okmen.zy@foxmail.com>
2017-06-13 17:14:05 +0800

    jobs

--- workspace/bbyiya-manager/src/main/java/com/bbyiya/cts/job/HeartbeatJob.java
+++ workspace/bbyiya-manager/src/main/java/com/bbyiya/cts/job/HeartbeatJob.java
@@ -1,5 +1,31 @@
 package com.bbyiya.cts.job;
 
-public class HeartbeatJob {
 
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.bbyiya.utils.HttpRequestHelper;
+
+public class HeartbeatJob extends QuartzJobBean {
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		this.work();
+	}
+
+	private void work() {
+		
+		send_mpic();
+	}
+
+	
+
+	private String send_mpic() {
+		String url = "https://mpic.bbyiya.com/login/otherLogin";
+		String dataParam = "headImg=zhangyao&loginType=2&nickName=111&openId=om_zCwqqNLeG-d39gC5llcDbbaic";
+		String result = HttpRequestHelper.sendPost(url, dataParam);
+		System.out.println(result); 
+		return result;
+	}
+	
 }

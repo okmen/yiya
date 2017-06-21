8a6887d12b29369a25ea0f60c3d6030d4d731e70
zhuliye0927 <290228628@qq.com>
2017-06-21 19:28:46 +0800

    zlp

--- workspace/bbyiya-manager/src/main/java/com/bbyiya/cts/job/HeartbeatJob.java
+++ workspace/bbyiya-manager/src/main/java/com/bbyiya/cts/job/HeartbeatJob.java
@@ -33,45 +33,56 @@ public class HeartbeatJob extends QuartzJobBean {
 	 * 公用job
 	 */
 	private void JobRun(){
-		//获取公用job的服务列表
-		List<Map<String, String>> joblist=ConfigUtil.getMaplist("jobs");
-		if(joblist!=null&&joblist.size()>0){
-			String keyBase=ConfigUtil.getSingleValue("currentRedisKey-Base")+"_job";
-			Date nowtime =new Date();
-			for (Map<String, String> job : joblist) {
-				if(ObjectUtil.parseInt(job.get("seton"))==1 ){
-					//服务的 keyid
-					String key=keyBase+"_"+job.get("id");
-					JobTime timeMod=  (JobTime)RedisUtil.getObject(key);
-					if(timeMod!=null&&timeMod.getLastTime()!=null){
-						long timeSpanLong= nowtime.getTime()-timeMod.getLastTime().getTime();
-						long timeSpan=timeSpanLong/1000;
-						if(timeSpan>ObjectUtil.parseLong(job.get("timespan"))){
-							//达到job的执行时间
-							if(ObjectUtil.parseInt(job.get("ispost"))==1){
-								HttpRequestHelper.sendPost(job.get("posturl"),""); 
-							}else{
-								//调去本地
-								doLocalServiceMothod(job.get("id"));
-							}
-							//达到job启动条件，执行job任务
-							timeMod.setLastTime(nowtime);
-							RedisUtil.setObject(key, timeMod);
-						}
-					}else {//刚进来时不执行job
-						timeMod=new JobTime();
-						timeMod.setLastTime(nowtime);
-						RedisUtil.setObject(key, timeMod);
-					}
-				}
-			}
+		
+		try {
+			autoOrderService.dotempAutoOrderSumbit();
+			Log.info("dotempAutoOrderSumbit执行自动下单操作完成！");
+		} catch (Exception e) {
+			Log.error("dotempAutoOrderSumbit方法执行出错！");
+			addSysLog("dotempAutoOrderSumbit方法执行出错！","dotempAutoOrderSumbit","自动下单");
 		}
+			
+		
+//		//获取公用job的服务列表
+//		List<Map<String, String>> joblist=ConfigUtil.getMaplist("jobs");
+//		if(joblist!=null&&joblist.size()>0){
+//			String keyBase=ConfigUtil.getSingleValue("currentRedisKey-Base")+"_job";
+//			Date nowtime =new Date();
+//			for (Map<String, String> job : joblist) {
+//				if(ObjectUtil.parseInt(job.get("seton"))==1 ){
+//					//服务的 keyid
+//					String key=keyBase+"_"+job.get("id");
+//					JobTime timeMod=  (JobTime)RedisUtil.getObject(key);
+//					if(timeMod!=null&&timeMod.getLastTime()!=null){
+//						long timeSpanLong= nowtime.getTime()-timeMod.getLastTime().getTime();
+//						long timeSpan=timeSpanLong/1000;
+//						if(timeSpan>ObjectUtil.parseLong(job.get("timespan"))){
+//							//达到job的执行时间
+//							if(ObjectUtil.parseInt(job.get("ispost"))==1){
+//								HttpRequestHelper.sendPost(job.get("posturl"),""); 
+//							}else{
+//								//调去本地
+//								doLocalServiceMothod(job.get("id"));
+//							}
+//							//达到job启动条件，执行job任务
+//							timeMod.setLastTime(nowtime);
+//							RedisUtil.setObject(key, timeMod);
+//						}
+//					}else {//刚进来时不执行job
+//						timeMod=new JobTime();
+//						timeMod.setLastTime(nowtime);
+//						RedisUtil.setObject(key, timeMod);
+//					}
+//				}
+//			}
+//		}
 	}
 
 	@Resource(name = "tempAutoOrderSumbitService")
 	private ITempAutoOrderSumbitService autoOrderService;
 	@Autowired
 	private SysLogsMapper syslogMapper;
+	
 	public void doLocalServiceMothod(String serviceId){	
 		try {
 			if(serviceId.equalsIgnoreCase("dotempAutoOrderSumbit")){

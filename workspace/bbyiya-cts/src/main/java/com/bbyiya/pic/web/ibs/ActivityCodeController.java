b2ac74290911733d80ea8811cfccddc96f32fc70
zhuliye0927 <290228628@qq.com>
2017-06-28 11:49:13 +0800

    zlp加活动码导出功能

--- workspace/bbyiya-cts/src/main/java/com/bbyiya/pic/web/ibs/ActivityCodeController.java
+++ workspace/bbyiya-cts/src/main/java/com/bbyiya/pic/web/ibs/ActivityCodeController.java
@@ -1,5 +1,12 @@
 package com.bbyiya.pic.web.ibs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
@@ -7,14 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bbyiya.baseUtils.ExportExcel;
 import com.bbyiya.enums.ReturnStatus;
 import com.bbyiya.pic.service.ibs.IIbs_ActivityCodeService;
+import com.bbyiya.pic.vo.product.ActivityCodeProductVO;
 import com.bbyiya.pic.vo.product.MyProductTempAddParam;
+import com.bbyiya.utils.FileUtils;
 import com.bbyiya.utils.JsonUtil;
 import com.bbyiya.utils.ObjectUtil;
 import com.bbyiya.vo.ReturnModel;
 import com.bbyiya.vo.user.LoginSuccessResult;
 import com.bbyiya.web.base.SSOController;
+import com.github.pagehelper.PageInfo;
+import com.sdicons.json.mapper.MapperException;
 
 @Controller
 @RequestMapping(value = "/ibs/temp")
@@ -116,6 +128,8 @@ public class ActivityCodeController extends SSOController {
 		return JsonUtil.objectToJsonStr(rq);
 	}
 	
+	
+	
 	/**
 	 * 删除活动码
 	 * @param tempId
@@ -167,6 +181,84 @@ public class ActivityCodeController extends SSOController {
 		rq=activitycodeService.resetAllTempApplySort(tempid);
 		return JsonUtil.objectToJsonStr(rq);
 		
+	}
+	
+	/**
+	 * 条形码excel导出
+	 * @param request
+	 * @return
+	 * @throws MapperException 
+	 */
+	@RequestMapping(value="/activityCodeProductExportExcel")
+	@ResponseBody
+	public String activityCodeProductExportExcel(Integer tempid,Integer activeStatus,
+			@RequestParam(required = false, defaultValue = "")String keywords) throws Exception {
+		// 列头
+		String[] headers =new String[10];
+		headers[0]="活动码";
+		headers[1]="对应产品";
+		headers[2]="活动状态";
+		headers[3]="客户昵称";
+		headers[4]="宝宝生日";
+		headers[5]="作品进度";
+		headers[6]="评论数";
+		headers[7]="客户手机";
+		headers[8]="开始制作时间";
+		headers[9]="最近更新时间";	
+		String[] fields = new String[10];
+		fields[0]="code.codeno";
+		fields[1]="productTitle";
+		fields[2]="activeStatus";
+		fields[3]="invitedName";
+		fields[4]="birthdayStr";
+		fields[5]="count";
+		fields[6]="commentsCount";
+		fields[7]="phone";
+		fields[8]="createtimestr";
+		fields[9]="updatetimestr";
+		
+		
+		//导出格式
+		String format =".xlsx";
+		ReturnModel rq = new ReturnModel();
+		LoginSuccessResult user = super.getLoginUser();
+		if (user != null) {
+			rq=activitycodeService.findMyProductslistForActivityCode(user.getUserId(), tempid,activeStatus,keywords, 0, 0);
+			
+			if(rq.getStatu()!=ReturnStatus.Success){
+				return JsonUtil.objectToJsonStr(rq);
+			}
+			
+			PageInfo<ActivityCodeProductVO> resultPage =(PageInfo<ActivityCodeProductVO>) rq.getBasemodle();
+			
+			List<ActivityCodeProductVO> list=resultPage.getList();
+			Long seed = System.currentTimeMillis();// 获得系统时间，作为生成随机数的种子
+			// 获取用户的当前工作主目录 
+			String sep=System.getProperty("file.separator");
+			String currentWorkDir = System.getProperty("user.home") +sep+ "imagedownloadtemp"+sep;
+			FileUtils.isDirExists(currentWorkDir);
+			ExportExcel<ActivityCodeProductVO> ex = new ExportExcel<ActivityCodeProductVO>();
+			String filename =  seed + format; ;
+			File file = new File(currentWorkDir + filename);	
+			try { 
+				OutputStream out = new FileOutputStream(file);				
+				ex.exportExcel("活动码", headers, fields, list, out, "yyyy-MM-dd");				
+				out.close();				
+				rq.setStatu(ReturnStatus.Success);
+				rq.setBasemodle(file.getPath());
+				return JsonUtil.objectToJsonStr(rq);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		} else {
+			rq.setStatu(ReturnStatus.LoginError);
+			rq.setStatusreson("登录过期");
+		}
+		return JsonUtil.objectToJsonStr(rq);
 	}
 		
 }

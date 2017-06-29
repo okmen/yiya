ce4ad8c46b7a52e37f3d500e648be5cce99c31bf
zhuliye0927 <290228628@qq.com>
2017-06-29 17:26:08 +0800

    zlp

---
+++ workspace/bbyiya-cts/src/main/java/com/bbyiya/pic/web/ibs/BranchAccountController.java
@@ -0,0 +1,53 @@
+package com.bbyiya.pic.web.ibs;
+
+
+import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.bbyiya.baseUtils.ValidateUtils;
+import com.bbyiya.enums.ReturnStatus;
+import com.bbyiya.enums.user.UserIdentityEnums;
+import com.bbyiya.service.IBaseUserAccountService;
+import com.bbyiya.utils.JsonUtil;
+import com.bbyiya.vo.ReturnModel;
+import com.bbyiya.vo.user.LoginSuccessResult;
+import com.bbyiya.web.base.SSOController;
+import com.sdicons.json.mapper.MapperException;
+
+@Controller
+@RequestMapping(value = "/ibs/account")
+public class BranchAccountController  extends SSOController{
+
+	
+	@Resource(name = "baseUserAccountService")
+	private IBaseUserAccountService accountService;
+	
+	
+	/**
+	 * 账户明细记录
+	 * @param branchuserid
+	 * @param amount
+	 * @return
+	 * @throws MapperException 
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/accountLog")
+	public String findAcountsLogsPageResult(Long userId, Integer type, int index, int size) throws MapperException {
+		ReturnModel rq=new ReturnModel();
+		LoginSuccessResult user= super.getLoginUser();
+		if(user!=null){
+			if(ValidateUtils.isIdentity(user.getIdentity(), UserIdentityEnums.branch)){
+				rq= accountService.findAcountsLogsPageResult(userId, type, index, size);
+			}else {
+				rq.setStatu(ReturnStatus.SystemError_1);
+				rq.setStatusreson("权限不足");
+			} 
+		}else {
+			rq.setStatu(ReturnStatus.LoginError);
+			rq.setStatusreson("登录过期");
+		}
+		return JsonUtil.objectToJsonStr(rq);
+	}
+}

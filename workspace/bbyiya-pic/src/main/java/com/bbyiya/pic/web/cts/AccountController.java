87ed14ffadf6db74a058e2e9fee5e7dc0c038bef
okmen <okmen.zy@foxmail.com>
2017-06-30 15:47:25 +0800

    合并0630-1

--- workspace/bbyiya-pic/src/main/java/com/bbyiya/pic/web/cts/AccountController.java
+++ workspace/bbyiya-pic/src/main/java/com/bbyiya/pic/web/cts/AccountController.java
@@ -2,27 +2,27 @@ package com.bbyiya.pic.web.cts;
 
 import java.util.Date;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bbyiya.baseUtils.GenUtils;
 import com.bbyiya.baseUtils.ValidateUtils;
 import com.bbyiya.dao.UAccountsMapper;
 import com.bbyiya.dao.UAdminMapper;
 import com.bbyiya.dao.UAdminactionlogsMapper;
 import com.bbyiya.dao.UCashlogsMapper;
 import com.bbyiya.dao.UUsersMapper;
-import com.bbyiya.enums.AccountLogType;
 import com.bbyiya.enums.AdminActionType;
+import com.bbyiya.enums.AmountType;
 import com.bbyiya.enums.ReturnStatus;
 import com.bbyiya.enums.user.UserIdentityEnums;
+import com.bbyiya.model.UAccounts;
 import com.bbyiya.model.UAdmin;
 import com.bbyiya.model.UAdminactionlogs;
+import com.bbyiya.model.UCashlogs;
 import com.bbyiya.model.UUsers;
-import com.bbyiya.service.IBaseUserAccountService;
 import com.bbyiya.utils.JsonUtil;
 import com.bbyiya.utils.ObjectUtil;
 import com.bbyiya.vo.ReturnModel;
@@ -44,25 +44,23 @@ public class AccountController  extends SSOController{
 	private UAdminactionlogsMapper actLogMapper;
 	@Autowired
 	private UAdminMapper adminMapper;
-	@Resource(name = "baseUserAccountService")
-	private IBaseUserAccountService accountService;
-	/**
-	 * cts 用户充值
-	 * @param branchuserid
-	 * @param amount
-	 * @return
-	 * @throws Exception
-	 */
-	@ResponseBody
-	@RequestMapping(value = "/chongzhi")
-	public String chongzhi(long branchuserid,String  amount) throws Exception {
-		ReturnModel rq = new ReturnModel();
-		LoginSuccessResult user=super.getLoginUser();
-		double amountPrice=ObjectUtil.parseDouble(amount);
-		if(user!=null) {
-			if(ValidateUtils.isIdentity(user.getIdentity(), UserIdentityEnums.cts_admin)||ValidateUtils.isIdentity(user.getIdentity(), UserIdentityEnums.cts_member)){
-				UUsers branch= userMapper.getUUsersByUserID(branchuserid);
-				if(branch!=null&&ValidateUtils.isIdentity(branch.getIdentity(), UserIdentityEnums.branch)) {
+//	/**
+//	 * cts 用户充值
+//	 * @param branchuserid
+//	 * @param amount
+//	 * @return
+//	 * @throws Exception
+//	 */
+//	@ResponseBody
+//	@RequestMapping(value = "/chongzhi")
+//	public String chongzhi(long branchuserid,String  amount) throws Exception {
+//		ReturnModel rq = new ReturnModel();
+//		LoginSuccessResult user=super.getLoginUser();
+//		double amountPrice=ObjectUtil.parseDouble(amount);
+//		if(user!=null) {
+//			if(ValidateUtils.isIdentity(user.getIdentity(), UserIdentityEnums.cts_admin)||ValidateUtils.isIdentity(user.getIdentity(), UserIdentityEnums.cts_member)){
+//				UUsers branch= userMapper.getUUsersByUserID(branchuserid);
+//				if(branch!=null&&ValidateUtils.isIdentity(branch.getIdentity(), UserIdentityEnums.branch)) {
 //					UAccounts accounts=accountMapper.selectByPrimaryKey(branchuserid);
 //					if(accounts!=null&&accounts.getAvailableamount()!=null&&accounts.getAvailableamount()>1000){
 //						if(amountPrice>10){
@@ -97,35 +95,24 @@ public class AccountController  extends SSOController{
 //						accounts.setAvailableamount(totalPriceTemp);
 //						accountMapper.insert(accounts);
 //					}
-					if(accountService.add_accountsLog(branchuserid, Integer.parseInt(AccountLogType.get_recharge.toString()), amountPrice, "9999", "")){
-						addActionLog(user.getUserId(),"[账户充值]操作成功！充值金额："+amountPrice+"元！充值用户 userId:"+branchuserid);
-						rq.setStatusreson("充值成功！成功充值："+amountPrice+"元!"); 
-						rq.setStatu(ReturnStatus.Success);
-					}else {
-						rq.setStatu(ReturnStatus.SystemError);
-						rq.setStatusreson("充值失败");
-					} 
-					
-				}else {
-					rq.setStatu(ReturnStatus.SystemError);
-					rq.setStatusreson("该用户不是影楼身份！"); 
-				}
-			}else {
-				rq.setStatu(ReturnStatus.SystemError);
-				rq.setStatusreson("权限不足！");
-			}
-		}else {
-			rq.setStatu(ReturnStatus.LoginError);
-			rq.setStatusreson("登录过期");
-		}
-		return JsonUtil.objectToJsonStr(rq);
-	}
-	
-	/**
-	 * cts操作日志
-	 * @param userId
-	 * @param msg
-	 */
+//					addActionLog(user.getUserId(),"[账户充值]操作成功！充值金额："+totalPriceTemp+"元！充值用户 userId:"+branchuserid);
+//					rq.setStatusreson("充值成功！账户可用金额："+accounts.getAvailableamount()+"元!"); 
+//					rq.setStatu(ReturnStatus.Success);
+//				}else {
+//					rq.setStatu(ReturnStatus.SystemError);
+//					rq.setStatusreson("该用户不是影楼身份！"); 
+//				}
+//			}else {
+//				rq.setStatu(ReturnStatus.SystemError);
+//				rq.setStatusreson("权限不足！");
+//			}
+//		}else {
+//			rq.setStatu(ReturnStatus.LoginError);
+//			rq.setStatusreson("登录过期");
+//		}
+//		return JsonUtil.objectToJsonStr(rq);
+//	}
+//	
 	public void addActionLog(Long userId,String msg){
 		UAdminactionlogs log=new UAdminactionlogs();
 		log.setUserid(userId);

f7031492523c2d95c46d19013418887e37c8bded
okmen <okmen.zy@foxmail.com>
2017-08-04 13:56:19 +0800

    店铺信息-手机绑定信息

--- workspace/bbyiya-pic/src/main/java/com/bbyiya/pic/web/version_one/ShopMgtController.java
+++ workspace/bbyiya-pic/src/main/java/com/bbyiya/pic/web/version_one/ShopMgtController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.bbyiya.dao.UBranchesMapper;
 import com.bbyiya.enums.ReturnStatus;
 import com.bbyiya.model.UBranches;
+import com.bbyiya.model.UUsers;
 import com.bbyiya.pic.vo.agent.BranchShopInfo;
 import com.bbyiya.utils.JsonUtil;
 import com.bbyiya.utils.ObjectUtil;
 import com.bbyiya.vo.ReturnModel;
+import com.bbyiya.vo.user.LoginSuccessResult;
 import com.bbyiya.web.base.SSOController;
 
 
@@ -31,17 +33,24 @@ public class ShopMgtController extends SSOController{
 	@RequestMapping(value = "/info")
 	public String shopInfo(long uid) throws Exception {
 		ReturnModel rq = new ReturnModel();
-		UBranches branch= branchMapper.selectByPrimaryKey(uid);
-		if(branch!=null){
-			BranchShopInfo shopInfo=new BranchShopInfo();
-			if(!ObjectUtil.isEmpty(branch.getLogo())){
-				shopInfo.setLogo(branch.getLogo());
+		LoginSuccessResult users=super.getLoginUser();
+		if(users!=null){
+			UBranches branch= branchMapper.selectByPrimaryKey(uid);
+			if(branch!=null){
+				BranchShopInfo shopInfo=new BranchShopInfo();
+				if(!ObjectUtil.isEmpty(branch.getLogo())){
+					shopInfo.setLogo(branch.getLogo());
+				}
+				if(!ObjectUtil.isEmpty(branch.getPromotionstr())){
+					shopInfo.setPromotionStr(branch.getPromotionstr());
+				}
+				if(!ObjectUtil.isEmpty(users.getMobilePhone())){
+					shopInfo.setMobileBind(1); 
+				}
+				rq.setBasemodle(shopInfo); 
 			}
-			if(!ObjectUtil.isEmpty(branch.getPromotionstr())){
-				shopInfo.setPromotionStr(branch.getPromotionstr());
-			}
-			rq.setBasemodle(shopInfo); 
 		}
+		
 		rq.setStatu(ReturnStatus.Success);
 		return JsonUtil.objectToJsonStr(rq);
 	}

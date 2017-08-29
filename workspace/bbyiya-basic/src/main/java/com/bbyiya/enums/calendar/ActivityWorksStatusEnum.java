c4e458e135cd3e7d91e39ffd0114a8525462fdd5
zhuliye0927 <290228628@qq.com>
2017-08-29 18:46:35 +0800

    enum submit

---
+++ workspace/bbyiya-basic/src/main/java/com/bbyiya/enums/calendar/ActivityWorksStatusEnum.java
@@ -0,0 +1,34 @@
+package com.bbyiya.enums.calendar;
+
+public enum ActivityWorksStatusEnum {
+	/**
+     * 已参与,已报名
+     */
+    apply(0),
+	/**
+     * 图片已提交
+     */
+    imagesubmit(1),
+    /**
+     * 已完成分享
+     */
+    completeshare(2),
+    
+    /**
+     * 已完成（已下单）
+     */
+    completeorder(3);
+    
+    private final int Type;
+
+    private ActivityWorksStatusEnum(int step)
+    {
+
+        this.Type = step;
+    }
+
+    public String toString()
+    {
+        return String.valueOf(this.Type);
+    }
+}

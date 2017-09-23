7e463f9bba954ab1efe9a6431a3b41b5d437e965
okmen <okmen.zy@foxmail.com>
2017-09-23 15:20:35 +0800

    登录session

--- workspace/bbyiya-basic/src/main/java/com/bbyiya/baseUtils/CookieUtils.java
+++ workspace/bbyiya-basic/src/main/java/com/bbyiya/baseUtils/CookieUtils.java
@@ -4,9 +4,16 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//import org.apache.log4j.Logger;
+
+import com.bbyiya.utils.ConfigUtil;
+import com.bbyiya.utils.RedisUtil;
+
 public class CookieUtils {
 
 	private static String COOKIE_NAME="photo_Cookie_token";
+//	private static Logger logger = Logger.getLogger(CookieUtils.class);
+	
 //	private static String PIC_COOKIE_NAME="Pic_Cookie_token";
 	
 //	/**
@@ -19,10 +26,14 @@ public class CookieUtils {
 //	public static void addCookieBySessionId(HttpServletRequest request, HttpServletResponse response, String value, int maxAge) {
 //		String sessionId= request.getSession().getId();
 //		Cookie cookie = new Cookie(sessionId, value);
-//		cookie.setPath("/");
-//		if (maxAge > 0)
+//		cookie.setPath(request.getContextPath()); 
+//		if (maxAge > 0){
 //			cookie.setMaxAge(maxAge);
+//		}else {
+//			cookie.setMaxAge(1800); 
+//		}
 //		response.addCookie(cookie);
+//		RedisUtil.setString(sessionId, value, maxAge); 
 //	}
 //	
 //
@@ -47,10 +58,25 @@ public class CookieUtils {
 	
 	public static void addCookie_web(HttpServletRequest request, HttpServletResponse response, String value, int maxAge) {		
 		Cookie cookie = new Cookie(COOKIE_NAME, value);
+		String currentDomain=ConfigUtil.getSingleValue("currentApiDomain");
+		//如果是测试环境
+		if(currentDomain.contains(".net")){
+			Cookie cookieNet = new Cookie(COOKIE_NAME, value);
+			cookieNet.setDomain(".bbyiya.net");
+			cookieNet.setPath("/");
+			if (maxAge > 0)
+				cookieNet.setMaxAge(maxAge);
+			response.addCookie(cookieNet);
+		}
+		cookie.setDomain(".bbyiya.com");
 		cookie.setPath("/");
 		if (maxAge > 0)
 			cookie.setMaxAge(maxAge);
 		response.addCookie(cookie);
+		//-----
+		RedisUtil.setString(request.getSession().getId(), value, maxAge); 
+//		logger.error("sessionId="+request.getSession().getId()+",value="+value);
+//		System.out.println("sessionId="+request.getSession().getId()+",value="+value);
 	}
 	
 	public static String getCookie_web(HttpServletRequest request) {
@@ -58,12 +84,19 @@ public class CookieUtils {
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
 				if (COOKIE_NAME.equals(cookie.getName().trim())) {
+//					logger.error("getCookie="+request.getSession().getId()+",value="+cookie.getValue());
+//					System.out.println("getCookie="+request.getSession().getId()+",value="+cookie.getValue());
 					return cookie.getValue();
 				}
 			}
-		} 
-		return null;
+		}
+		
+		String token=  RedisUtil.getString(request.getSession().getId());
+//		logger.error("getSession="+request.getSession().getId()+",value="+token);
+//		System.out.println("getSession="+request.getSession().getId()+",value="+token);
+		return token;
 		
 	}
+	
 	
 }
